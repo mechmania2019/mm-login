5d5e9c3813fee9548bb7747c7a9d5fad5782f52b
angiecheng <angie.cheng33@gmail.com>
2019-08-24 18:50:39 -0500

    made changes for deployment

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const authenticate = require('mm-authenticate')(mongoose)
 mongoose.connect(process.env.MONGO_URL)
 mongoose.Promise = global.Promise
 
-module.exports = authenticate(async (req, res) => req.user)
+module.exports = authenticate(async (req, res) => res.end(JSON.stringify(req.user)))
